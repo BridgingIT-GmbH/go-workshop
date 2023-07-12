a1b91f3635650115b9de534a176392b3278a4d4d
Marcel Schneider <marcel.schneider@bridging-it.de>
2023-07-12 13:15:01 +0200

    adjusted port for go app

--- moviedb-go/config/parameter.go
+++ moviedb-go/config/parameter.go
@@ -3,7 +3,7 @@ package config
 import "os"
 
 var MongoUri = getParam("MONGO_URI", "mongodb://localhost:27017")
-var ServerHostPort = getParam("SERVER_HOST_PORT", "localhost:8080")
+var ServerHostPort = getParam("SERVER_HOST_PORT", "localhost:8090")
 
 func getParam(key string, defaultValue string) string {
 	value, exists := os.LookupEnv(key)
